9c789028b79985df9da26c06c76432476462c323
IYUANWEIZE <iyuanweize@gmail.com>
2022-08-04 02:32:24 +0200

    pip3 check

--- rulelist_add.py
+++ rulelist_add.py
@@ -4,15 +4,17 @@ def import_yaml():
     try:
         import yaml
     except ImportError:
-        os.system("pip3 install pyyaml")
+        if "command not found" in os.popen("pip3"):
+            print("yes")
+        # os.system("apt install python3-pip&&pip3 install pyyaml")
 
 def add_rule():
     if os.path.exists(r"/etc/XrayR/config.yml")==True:
         print("yes")
     else:
         print(sys.modules)
-    print(sys.modules)
+    # print(sys.modules)
 
 if __name__=="__main__":
     import_yaml()
-    add_rule()
+    # add_rule()
