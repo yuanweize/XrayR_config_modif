08e4eaa84bb6f1e194ffda6f4d877de857dc2bb8
IYUANWEIZE <iyuanweize@gmail.com>
2022-08-04 02:22:08 +0200

    yaml modules chek

--- rulelist_add.py
+++ rulelist_add.py
@@ -1,10 +1,18 @@
-import os,yaml
+import os
+import sys
+def import_yaml():
+    try:
+        import yaml
+    except ImportError:
+        os.system("pip3 install pyyaml")
+
 def add_rule():
     if os.path.exists(r"/etc/XrayR/config.yml")==True:
         print("yes")
     else:
-        print("no")
-
+        print(sys.modules)
+    print(sys.modules)
 
 if __name__=="__main__":
+    import_yaml()
     add_rule()
